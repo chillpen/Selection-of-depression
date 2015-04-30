95014d8a3942d8aac0242b8bada1e73b49700169
chillpen <chillpen@chillpen-virtual-machine>
2015-04-30 16:50:03 +0800

    20150430

---
+++ SelUnit.py
@@ -0,0 +1,19 @@
+
+import math
+from struct import  *
+from array import array
+
+
+class SelUnit(object):
+    
+    X =0
+    Y =0
+    def __init__(self, x,y):
+            '''
+            Constructor
+            '''
+            self.X = x
+            self.Y = y
+     
+
+     
